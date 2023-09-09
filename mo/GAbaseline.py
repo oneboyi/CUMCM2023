39df3cec34914df26478a73fe5f4fdfa7fee4610
MarasyZZ <3173312535@qq.com>
2023-09-10 03:51:56 +0800

    111

--- mo/GAbaseline.py
+++ mo/GAbaseline.py
@@ -200,25 +200,21 @@ def calculate_length(beta, d_0, Dc):
             sumWE += o
             if sumWE > (sqrt(3) * d_max(Dc)) / cos(beta):
                 L += (sqrt(3) * d_max(Dc)) / (cos(beta) * sin(beta))
+    return L
 
-def fitness_continuous_coverage(chromosome):
-    # total_length = 0
-    # last_line = 0
-    # for line in chromosome:
-    #     coverage_width = 2 * (D_cen + (line - width/2) * np.tan(alpha)) * np.tan(theta/2)
-    #     overlap = coverage_width - (line - last_line)
-    #     if overlap < d_min or overlap > d_max:
-    #         return 0  # 无效的解
-    #     total_length += line - last_line
-    #     last_line = line
-    #
-    # # 如果最后一个测线没有覆盖到东端，适应度为0
-    # if last_line + coverage_width < width:
-    #     return 0
+def fitness_continuous_coverage(chromosome,index):
+    beta=chromosome[0]
+    d_0=chromosome[1]
+    len=calculate_length(beta, d_0, Dc)
+    value=[]
+    value.append(len)
+    value.append(index)
+    return value
 
-    return 0
 
 def crossover(parent1, parent2):
+    print(len(parent1))
+    print(len(parent2))
     idx = np.random.randint(1, len(parent1)-1)
     child1 = np.concatenate((parent1[:idx], parent2[idx:]))
     child2 = np.concatenate((parent2[:idx], parent1[idx:]))
@@ -236,25 +232,27 @@ def initialize_population():
     for _ in range(population_size):
         angle = np.random.randint(1000, 89000)
         d = np.random.randint(45000,63000)
-        one = (angle/1000,d/1000)
+        one=[]
+        one.append(angle/1000)
+        one.append(d/1000)
         initial_population.append(one)
     return np.array(initial_population)
 
 def enhanced_genetic_algorithm_continuous_coverage():
-#     population = initialize_population()
-#     for generation in range(num_generations):
-#         fitness_values = [fitness_continuous_coverage(chromo) for chromo in population]
-#         elite_indices = np.argsort(fitness_values)[-elite_size:]
-#         new_population = [population[i] for i in elite_indices]
-#         while len(new_population) < population_size:
-#             parents = np.argsort(fitness_values)[-2:]
-#             child1, child2 = crossover(population[parents[0]], population[parents[1]])
-#             new_population.extend([child1, child2])
-#             for chromo in new_population[-2:]:
-#                 adaptive_mutation(chromo, fitness_continuous_coverage(chromo))
-#         population = np.array(new_population)
-#     return population[np.argmax([fitness_continuous_coverage(chromo) for chromo in population])]
-#
-# best_solution_continuous_coverage = enhanced_genetic_algorithm_continuous_coverage()
-# best_solution_continuous_coverage
-    return 0
+    population = initialize_population()
+    for generation in range(num_generations):
+        fitness_values = [fitness_continuous_coverage(chromo,index) for index,chromo in enumerate(population)]
+
+        elite_indices = np.argsort(fitness_values[:][0])[-elite_size:]
+        new_population = [population[i[1]] for i in elite_indices]
+        while len(new_population) < population_size:
+            parents = np.argsort(fitness_values)[-2:]
+            child1, child2 = crossover(population[parents[0]], population[parents[1]])
+            new_population.extend([child1, child2])
+            for chromo in new_population[-2:]:
+                adaptive_mutation(chromo, fitness_continuous_coverage(chromo))
+        population = np.array(new_population)
+    return population[np.argmax([fitness_continuous_coverage(chromo) for chromo in population])]
+
+best_solution_continuous_coverage = enhanced_genetic_algorithm_continuous_coverage()
+print(best_solution_continuous_coverage)
